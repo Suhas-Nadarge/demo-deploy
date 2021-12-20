cf5cc195f95064c65ce41c905a550541e22ca401
Suhas Nadarge <suhas.nadarge@mnx.com>
2021-12-20 18:03:17 +0000

    updates-5.0

--- server.js
+++ server.js
@@ -11,5 +11,10 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+  });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
